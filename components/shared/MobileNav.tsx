53b1be554ddb9934401ec964ad524884cfba148f
OlatunjiAbiodunEmmanuel <olatunjia889@gmail.com>
2024-01-26 21:57:07 +0100

    add color

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -9,6 +9,7 @@ import {
 import Image from "next/image"
 import { Separator } from "../ui/separator"
 import NavItems from "./NavItems"
+import { MenuIcon } from "lucide-react"
 
 
 const MobileNav = () => {
@@ -16,13 +17,14 @@ const MobileNav = () => {
     <nav className="md:hidden">
       <Sheet>
         <SheetTrigger className="align-middle">
-          <Image 
+          {/* <Image 
             src="/assets/icons/menu.svg"
             alt="menu"
             width={24}
             height={24}
             className="cursor-pointer"
-          />
+          /> */}
+          <MenuIcon/>
         </SheetTrigger>
         <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
           <Image 
